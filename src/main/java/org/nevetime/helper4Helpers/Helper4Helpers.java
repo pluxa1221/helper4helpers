b824791b722c22ed7aa5fd2635e0aff529dd7ca5
pluxa1221 <prudn1kov.misha@yandex.ru>
2026-09-12 09:46:31 +0700

    Added luckperms as dependency and getter in main class

--- src/main/java/org/nevetime/helper4Helpers/Helper4Helpers.java
+++ src/main/java/org/nevetime/helper4Helpers/Helper4Helpers.java
@@ -1,19 +1,32 @@
 package org.nevetime.helper4Helpers;
 
+import net.luckperms.api.LuckPerms;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Helper4Helpers extends JavaPlugin {
 
     public Helper4Helpers instance = this;
 
+    private LuckPerms luckperms;
+
     @Override
     public void onEnable() {
         // Plugin startup logic
+        RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+        if (provider != null) {
+            luckperms = provider.getProvider();
+        }
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public LuckPerms getLuckPerms() {
+        return luckperms;
     }
 
     public Helper4Helpers getInstance() {
